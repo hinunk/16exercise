531cfabccdf3ffb110b8384ece12aa8192f6d59e
hinunk <526996400@qq.com>
2017-04-01 17:28:29 +0800

    第一次

---
+++ 16exercise.js
@@ -0,0 +1,8 @@
+generate.onclick = function () {
+  cm1.innerText = alter1.value
+  cm2.innerText = alter2.value
+  cm3.innerText = alter3.value
+  cm4.innerText = alter4.value
+  cm5.innerText = alter5.value
+  cm6.innerText = alter6.value
+}
