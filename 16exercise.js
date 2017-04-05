fd7914b7db784640b51d18133e91cf42351c5934
hinunk <526996400@qq.com>
2017-04-05 19:46:53 +0800

    2-3

--- 16exercise.js
+++ 16exercise.js
@@ -1,8 +1,20 @@
 generate.onclick = function () {
-  cm1.innerText = alter1.value
-  cm2.innerText = alter2.value
-  cm3.innerText = alter3.value
-  cm4.innerText = alter4.value
-  cm5.innerText = alter5.value
-  cm6.innerText = alter6.value
+  // cm1.innerText = alter1.value
+  // cm2.innerText = alter2.value
+  // cm3.innerText = alter3.value
+  // cm4.innerText = alter4.value
+  // cm5.innerText = alter5.value
+  // cm6.innerText = alter6.value
+  
+var html = `<pre>
+抵制不良 ${alter1.value} ，拒绝盗版 ${alter2.value} 。
+注意自我 ${alter3.value} ，谨防上当 ${alter4.value} 。
+适度 ${alter5.value} 益脑，沉迷 ${alter6.value} 伤身。
+合理安排时间，享受健康生活。
+    </pre>`
+     content.innerHTML = html
+
+
+
+     
 }
